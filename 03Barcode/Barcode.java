c9a0afcd0d9da64ff267e0843a7db74b8e225585
doniatung <doniatung32@gmail.com>
2016-12-08 22:40:02 -0500

    new n improved except i didnt test yet

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -18,15 +18,7 @@ public class Barcode implements Comparable<Barcode>{
       }
   }
 
-      
-
-// postcondition: Creates a copy of a bar code.
-  public Barcode clone(){
-      Barcode clonedBc = new Barcode(_zip);
-      return clonedBc; 
-  }
-
-
+     
 // postcondition: computes and returns the check sum for _zip
   private int checkSum(){
       int ans = 0;
@@ -36,39 +28,13 @@ public class Barcode implements Comparable<Barcode>{
       return ans;
   }
 
+
+    
+
 //postcondition: format zip + check digit + Barcode 
 //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
   public String toString(){
-      String ans = _zip + checkDigit + "  |";
-      for (int i = 0; i < _zip.length(); i ++){
-	  int num = _zip.charAt(i);
-	  switch (num){
-	  case 1: ans += ":::||";
-	      break;
-          case 2: ans += "::|:|";
-              break;
-          case 3: ans += "::||:";
-              break;
-          case 4: ans += ":|::|";
-              break;
-          case 5: ans += ":|:|:";
-              break;
-          case 6: ans += ":||::";
-              break;
-          case 7: ans += "|:::|";
-              break;
-          case 8: ans += "|::|:";
-              break;
-          case 9: ans += "|:|::";
-	     break;
-	  case 0: ans += "||:::";
-	      break;
-	  default: ans += "smthg";
-	      break;
-	  }
-      }
-      return ans;
-
+      return _zip + checkDigit + "   " + toCode(_zip);
   }
 
 
@@ -79,5 +45,88 @@ public class Barcode implements Comparable<Barcode>{
       
       return thisZip.compareTo(otherZip);
 	  }
+
+    public static String toCode(String zip){
+	if (zip.length() != 5){
+	    throw new IllegalArgumentException();
+	}
+	else{
+	    String ans = "|";
+	    for (int i = 0; i < zip.length(); i ++){
+		int num = zip.charAt(i);
+		switch (num){
+		case 1: ans += ":::||";
+		    break;
+		case 2: ans += "::|:|";
+		    break;
+		case 3: ans += "::||:";
+		    break;
+		case 4: ans += ":|::|";
+		    break;
+		case 5: ans += ":|:|:";
+		    break;
+		case 6: ans += ":||::";
+		    break;
+		case 7: ans += "|:::|";
+		    break;
+		case 8: ans += "|::|:";
+		    break;
+		case 9: ans += "|:|::";
+		    break;
+		case 0: ans += "||:::";
+		    break;
+		default: ans += "smthg";
+		    break;
+		}
+	    }
+	}
+      return ans + "|";
+    }
+
+
+    public static String toZip(String code){
+	if (code.length() != 32){
+	    throw new IllegalArgumentException("Please make sure you have 32 characters in your code");
+	}
+	if (code.charAt(0) != "|"){
+	    throw new IllegalArgumentException("Please make sure your code starts with a bar");
+	}
+	//if (checkSum fails) I'm not sure if checkSum is supposed to be printed or not
+	//invalid characters?
+	else{
+	    String zip = "";
+	    int i = 1;
+	    while (i < code.length()-11){
+		String numCode = code.substring(i,i+5);
+		switch(numCode){
+		case  ":::||": zip += 1;
+		    break;
+		case "::|:|": zip += 2;
+		    break;
+		case "::||:": zip += 3;
+		    break;
+		case ":|::|": zip += 4;
+		    break;
+		case ":|:|:": zip += 5;
+		    break;
+		case ":||::": zip += 6;
+		    break;
+		case "|:::|": zip += 7;
+		    break;
+		case "|::|:": zip += 8;
+		    break;
+		case "|:|::": zip += 9;
+		    break;
+		case "||:::": zip += 0;
+		    break;
+		default: throw new IllegalArgumentException("Please make sure you don't have a pattern mismatch");
+		    break;
+		}
+        	i += 5;
+	    }
+	}
+	return ans;
+    }
+
     
 }
